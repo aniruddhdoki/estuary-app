074e7abfc3266093907551340ea0d90cc6fea5bc
Aniruddh Doki <doki.aniruddh@gmail.com>
2023-04-18 17:05:31 -0400

    Initial Commit

---
+++ estuary-app/estuary_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  estuary_appApp.swift
+//  estuary-app
+//
+//  Created by Aniruddh Doki on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct estuary_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
